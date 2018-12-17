43b024cd2ddf3620af738efeac569738ca5663f9
Kyle Laster <kyle.laster@gmail.com>
2018-12-17 12:00:48 -0600

    added gameover

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,34 @@
 
-let characters = [1, 2, 3, 4];
+let characters = [[1,45],[2, 80], [3, 110], [4, 150]];
 let hero = [];
-let enemiesArr = ['R2', 'Vader', 'Trooper', 'Chewbacca']
+let nemesis = [];
+let enemiesArr = [['R2-D2', 45], ['Trooper', 80], ['Vader', 110], ['Chewbacca', 150]];
 let count = 0;
 let enemyCount = 0;
 let damage = 0;
-let enemydamage = 0;
+let enemyDamage = 0;
+let enHealth = 0;
+let myHealth = 0;
+let test = 0;
+let herotest = 0;
 
 
 const chooseHero = (props) => {
     if (count < 1) {
         characters.forEach(function(cur) {
-            if (cur != props) {
-                var enemies = document.querySelectorAll(`.a${cur}`);
+            if (cur[0] != props) {
+                var enemies = document.querySelectorAll(`.a${cur[0]}`);
                 enemies.forEach(function(i) {
                     i.classList.toggle("disapear");
             });
             } else {
+
+                myHealth = cur[1]
                 hero.push(props);
+                herotest = document.querySelector(`.health${cur[0]}`);
+                herotest.innerHTML = myHealth;                
+                console.log(hero[0]);
+
             };
         }); 
         count++
@@ -26,26 +37,47 @@ const chooseHero = (props) => {
 };
 
 
+
 const chooseEnemy = (props) => {
     if (enemyCount < 1) {
     enemiesArr.forEach(function(cur) {
-        if (cur == props) {
-            var enemies = document.querySelectorAll(`.a${cur}`);
+        if (cur[0] == props) {
+            enHealth = cur[1];
+            test = document.querySelector(`.enemyhealth-${cur[0]}`);
+            test.innerHTML = enHealth;
+            assignName();
+            var enemies = document.querySelectorAll(`.${cur[0]}`);
             enemies.forEach(function(i) {
                 i.classList.toggle("disapear");
             });
-        } 
+        }  else {
+            nemesis.push(props);
+            assignName();
+        }
     });
     enemyCount++
 };
 };
 
-const r2Attack = (min,max) => {
+const assignName = () => {
+    var nemesisArr = document.querySelectorAll(`.nemesis`);
+    nemesisArr.forEach(function(cur) { 
+        cur.parentElement.classList.toggle(`disapear`);
+        cur.innerHTML = `${nemesis[0]} `;
+
+});
+};
+
+
+//Normal attack type, random number within a range.
+const normAttack = (min,max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+
+//Trooper attack type, because they suck at hitting their target.
 const trooperAttack = () => {
     let windAffect = Math.floor(Math.random()*10);
     let wateryEyes = Math.floor(Math.random()*10);
@@ -57,32 +89,97 @@ const trooperAttack = () => {
     }
 };
 
-const vaderAttack = (min,max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const chewyAttack = (min,max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-
-document.querySelector(".hit").addEventListener("mousedown",function(){
+// You Attack
+document.querySelector(".hit").addEventListener("mousedown",function rainbow(){
     if (hero[0] === "1") {
-        console.log(r2Attack(50,75));
+        let strike = normAttack(50,75);
+        damage += strike;
+        enHealth -= strike;
+        test.innerHTML = enHealth;
+        console.log(enHealth);
+        return strike;
     } else if (hero[0] === "2") {
-        console.log(trooperAttack())
+        let strike = trooperAttack();
+        damage += strike;
+        enHealth -= strike;
+        test.innerHTML = enHealth;
+        console.log(enHealth);
+        return strike;
     } else if (hero[0] === "3") {
-        console.log(vaderAttack(35,120))
+        let strike = normAttack(35, 120);
+        damage += strike;
+        enHealth -= strike;
+        test.innerHTML = enHealth;
+        console.log(enHealth);
+        return strike;
     } else {
-        console.log(chewyAttack(1, 100))
+        let strike = normAttack(1,150);
+        damage += strike;
+        enHealth -= strike;
+        test.innerHTML = enHealth;
+        console.log(enHealth);
+        return strike;
     }
    });
 
-const Attack = () => {
+
+// Enemy Attacks
+   document.querySelector(".hit").addEventListener("mousedown",function(){
+    if (nemesis[0] === "1") {
+        let strike = normAttack(50,75);
+        enemyDamage += strike;
+        myHealth -= strike;
+        herotest.innerHTML = myHealth;
+        gameOver();
+    } else if (nemesis[0] === "2") {
+        let strike = trooperAttack();
+        enemyDamage += strike;
+        myHealth -= strike;
+        herotest.innerHTML = myHealth;
+        gameOver();
+    } else if (nemesis[0] === "3") {
+        let strike = normAttack(35,120);
+        enemyDamage += strike;
+        myHealth -= strike;
+        herotest.innerHTML = myHealth;
+        gameOver();
+    } else {
+        let strike = normAttack(1,150);
+        enemyDamage += strike;
+        myHealth -= strike;
+        herotest.innerHTML = myHealth;
+        gameOver();
+    }
+   });
+
+function gameOver() {
+    if (myHealth <= 0) {
+        document.getElementById(hero[0]).classList.toggle('disapear');
+        var h1 = document.createElement("h1");
+        h1.innerHTML = 'Game Over';
+        h1.classList.add('red');
+        document.querySelector('.lineup').appendChild(h1);
+
+        var reset = document.createElement("button");
+        reset.innerHTML="reset";
+        document.querySelector('.reset').appendChild(reset);
+        reset.classList.add('refresh');
+        document.querySelector('.refresh').addEventListener("mousedown", function (){
+            window.location.reload();
+        });
+        
+
+    }
+}
+
+
+
+const hpTracker = () => {
+
+}
+
+
+const enemyHpTracker = () => {
 
 };
 
