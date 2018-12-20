c956e6a11d6a89ab14e9e969a47baca965ce4bfb
Kyle Laster <kyle.laster@gmail.com>
2018-12-20 10:10:58 -0600

    added health bars and other bug fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,32 @@
 
-let characters = [[1,45],[2, 80], [3, 1100], [4, 150]];
+let characters = [[1,450],[2, 800], [3, 1100], [4, 1500]];
 let hero = [];
 let nemesis = [];
-let enemiesArr = [['R2-D2', 45], ['Trooper', 80], ['Vader', 110], ['Chewbacca', 150]];
+let enemiesArr = [['R2-D2', 450], ['Trooper', 800], ['Vader', 1100], ['Chewbacca', 1500]];
 let deadCount = 0;
 let count = 0;
 let enemyCount = 0;
 let damage = 0;
+let initialHealth = 0;
 let enemyDamage = 0;
 let enHealth = 0;
 let myHealth = 0;
 let test = 0;
 let herotest = 0;
-var e = 0;
+let e = 0;
+let ticker = 0;
+let resetCount = 0;
+
+let myHealthBar = document.getElementsByClassName("progress-bar")[0];
+let theirHealthBar = document.getElementsByClassName("progress-bar")[1];
+
+let percentageHealth = function(lifeLeft, lifeStart) {
+    healthPercentage = (lifeLeft/lifeStart) * 300;
+    return healthPercentage; 
+}
+
+
+
 
 function iChooseYou(props) {
 var chosenMonster = document.querySelector(`.a${props}`);
@@ -30,13 +44,11 @@ chosenImage.classList.add("resize");
 function iChooseThee(props) {
     var chosenMonster = document.querySelector(`.active-${props}`);
     chosenMonster.classList.toggle("enemySelected");
-    console.log(chosenMonster);
     var hideMe = chosenMonster.querySelectorAll("h3");
     hideMe.forEach(function(cur) {
         cur.classList.toggle("hide");
     var chosenImage = chosenMonster.querySelector("img");
     chosenImage.classList.add("resize");
-    console.log(chosenImage);
     });
     };
 
@@ -54,7 +66,8 @@ const chooseHero = (props) => {
             });
             } else {
                 iChooseYou(props);
-                myHealth = cur[1]
+                myHealth = cur[1];
+                heroHealth = cur[1];
                 hero.push(props);
                 herotest = document.querySelector(`.health${cur[0]}`);
                 herotest.innerHTML = myHealth;                
@@ -72,9 +85,9 @@ const chooseEnemy = (props) => {
     if (enemyCount < 1) {
     enemiesArr.forEach(function(cur) {
         if (cur[0] == props) {
-            console.log(props);
             iChooseThee(props);
             enHealth = cur[1];
+            initialHealth = cur[1];
             test = document.querySelector(`.enemyhealth-${cur[0]}`);
             test.innerHTML = enHealth;
             assignName();
@@ -84,6 +97,7 @@ const chooseEnemy = (props) => {
             var bullpin = document.querySelectorAll(`.${cur[0]}`);
             bullpin.forEach(function(i) {
                 i.classList.toggle("disapear");
+            myHealthBar.style.width = `${percentageHealth(enHealth, initialHealth)}px`
             });
         });
         }  else {
@@ -94,6 +108,7 @@ const chooseEnemy = (props) => {
     nemesis.push(props);
     }
     enemyCount++
+    ticker = 1;
 };
 };
 
@@ -129,38 +144,45 @@ const trooperAttack = () => {
 
 // You Attack
 document.querySelector(".hit").addEventListener("mousedown",function rainbow(){
-    if (hero[0] === "1") {
-        let strike = normAttack(50,75);
-        damage += strike;
-        enHealth -= strike;
-        test.innerHTML = enHealth;
-        nextEnemy();
-        return strike;
-    } else if (hero[0] === "2") {
-        let strike = trooperAttack();
-        damage += strike;
-        enHealth -= strike;
-        test.innerHTML = enHealth;
-        nextEnemy();
-        return strike;
-    } else if (hero[0] === "3") {
-        let strike = normAttack(35, 120);
-        damage += strike;
-        enHealth -= strike;
-        test.innerHTML = enHealth;
-        nextEnemy();
-        return strike;
-    } else {
-        let strike = normAttack(1,150);
-        damage += strike;
-        enHealth -= strike;
-        test.innerHTML = enHealth;
-        nextEnemy();
-        return strike;
+    if (resetCount === 0) {
+        if (hero[0] === "1") {
+            let strike = normAttack(50,75);
+            damage += strike;
+            enHealth -= strike;
+            test.innerHTML = enHealth;
+            nextEnemy();
+            myHealthBar.style.width = `${percentageHealth(enHealth, initialHealth)}px`
+            return strike;
+        } else if (hero[0] === "2") {
+            let strike = trooperAttack();
+            damage += strike;
+            enHealth -= strike;
+            test.innerHTML = enHealth;
+            nextEnemy();
+            myHealthBar.style.width = `${percentageHealth(enHealth, initialHealth)}px`
+            return strike;
+        } else if (hero[0] === "3") {
+            let strike = normAttack(35, 120);
+            damage += strike;
+            enHealth -= strike;
+            test.innerHTML = enHealth;
+            nextEnemy();
+            myHealthBar.style.width = `${percentageHealth(enHealth, initialHealth)}px`
+            return strike;
+        } else {
+            let strike = normAttack(1,150);
+            damage += strike;
+            enHealth -= strike;
+            test.innerHTML = enHealth;
+            nextEnemy();
+            myHealthBar.style.width = `${percentageHealth(enHealth, initialHealth)}px`
+            return strike;
     }
+}
    });
 
 function nextEnemy() {
+    if (ticker === 1) {
     if (enHealth <= 0) {
         let deadGuy = document.getElementById(`active-${nemesis[e]}`);
         e++;
@@ -169,49 +191,66 @@ function nextEnemy() {
         damage = 0;
         victory();
         enemyCount = 0;
-
+        ticker++
+    }
     }
 }
 
 
 // Enemy Attacks
    document.querySelector(".hit").addEventListener("mousedown",function(){
-       console.log(nemesis[0]);
+       if (ticker === 1) {
     if (nemesis[0] === "R2-D2") {
         let strike = normAttack(50,75);
         enemyDamage += strike;
         myHealth -= strike;
         herotest.innerHTML = myHealth;
         gameOver();
+        theirHealthBar.style.width = `${percentageHealth(myHealth, heroHealth)}px`
     } else if (nemesis[0] === "Trooper") {
         let strike = trooperAttack();
         enemyDamage += strike;
         myHealth -= strike;
         herotest.innerHTML = myHealth;
         gameOver();
+        theirHealthBar.style.width = `${percentageHealth(myHealth, heroHealth)}px`
     } else if (nemesis[0] === "Vader") {
         let strike = normAttack(35,120);
         enemyDamage += strike;
         myHealth -= strike;
         herotest.innerHTML = myHealth;
         gameOver();
+        theirHealthBar.style.width = `${percentageHealth(myHealth, heroHealth)}px`
+
     } else {
         let strike = normAttack(1,150);
         enemyDamage += strike;
         myHealth -= strike;
         herotest.innerHTML = myHealth;
         gameOver();
+        theirHealthBar.style.width = `${percentageHealth(myHealth, heroHealth)}px`
+
     }
+
+};
+
    });
 
 function gameOver() {
+console.log(myHealth);
+console.log(resetCount);
     if (myHealth <= 0) {
+        if (resetCount === 0) {
+        console.log(resetCount);
+        heroHealth = 300;
+        myHealth = 0;
+        theirHealthBar.style.width = `${percentageHealth(myHealth, heroHealth)}px`
         document.getElementById(hero[0]).classList.toggle('disapear');
         var h1 = document.querySelector("h1");
         h1.innerHTML = 'Game Over';
         h1.classList.add('red');
         document.querySelector('.lineup').appendChild(h1);
-
+        resetCount++;
         var reset = document.createElement("button");
         reset.innerHTML="Try Again";
         reset.classList.add("hit");
@@ -220,10 +259,11 @@ function gameOver() {
         document.querySelector('.refresh').addEventListener("mousedown", function (){
             window.location.reload();
         });
+    };
+        };
         
 
-    }
-};
+    };
 
 
 
@@ -247,14 +287,4 @@ const victory = () => {
 };
 
 
-
-
-
-
-
-
-
-
-//Find bug when clicking out of order.
-//Add healthbar
 //Add sound
